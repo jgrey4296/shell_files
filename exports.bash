56cf8c11407e020a1b945a290318e4c3845c4221
John Grey <jgrey@ucsc.edu>
2017-01-31 13:06:57 +0000

    main exports and path settings

---
+++ exports.bash
@@ -0,0 +1,38 @@
+#PATHS:
+export PATH=/Users/jgrey/node_modules:$PATH
+export PATH=/usr/local/bin:$PATH
+export PATH=/Applications/SuperCollider:$PATH
+export PATH=/Volumes/Documents/Dropbox/Programming/Perl:$PATH
+export PATH=/opt/local/bin:$PATH
+export PATH=/opt/local/sbin:$PATH
+export PATH=/opt/local/lib:$PATH
+export PATH=/opt/local/include:$PATH
+export PATH=/opt/local/libexec/gnubin:$PATH
+export PATH=/Volumes/Documents/github/jg_shell_files:$PATH
+export PATH=/Applications/Racket\ v6.1.1/bin:$PATH
+export PATH=/usr/local/go/bin:$PATH
+export PATH=/Users/jgrey/github/otherLibs/cppcheck:$PATH
+
+#For supercollider:
+export PATH=/Applications/SuperCollider/SuperCollider.app/Contents/Resources:$PATH
+export PATH=/Applications/SuperCollider/SuperCollider.app/Contents/MacOS:$PATH
+
+#racket:
+export PATH=/Applications/Racket/bin:$PATH
+
+#for latex (mactex, when installed):
+export PATH=/usr/local/texlive/2016/bin/x86_64-darwin:$PATH
+
+#for anaconda:
+export PATH=~/anaconda/bin:$PATH
+
+#for blender:
+export PATH=/Applications/blender/blender.app/Contents/MacOS:$PATH
+
+#setting up the prompt:
+export PS1='(\j): \D{%D} \A \u:  '
+
+#android sdk stuff:
+# export PATH=/Volumes/Documents/Libraries/android-sdk-macosx/build-tools/19.1.0:$PATH
+# export PATH=/Volumes/Documents/Libraries/android-sdk-macosx/platform-tools:$PATH
+# export PATH=/Volumes/Documents/Libraries/android-sdk-macosx/tools:$PATH

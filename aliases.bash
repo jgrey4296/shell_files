7fa9abd128bf81ae5fc1bb9c93adb43685f270ab
John Grey <jgrey@ucsc.edu>
2017-06-01 16:07:10 +0100

    removed language flags from the haskell alias, into the .ghci file

--- aliases.bash
+++ aliases.bash
@@ -79,6 +79,6 @@ alias wemacs="emacs ~/github/writing/main.org"
 
 #haskell:
 #don't forget to update emacs haskell program name variable when modifying this
-alias whaskell="ghci -XFlexibleContexts -Wall -fwarn-name-shadowing "
-alias haskell="ghci -XFlexibleContexts -XGeneralizedNewtypeDeriving -XScopedTypeVariables -ghci-script ~/.shell_files/.ghci_alt"
+alias whaskell="ghci -Wall -fwarn-name-shadowing -ghci-script ~/.shell_files/.ghci_alt"
+alias haskell="ghci -ghci-script ~/.shell_files/.ghci_alt"
 alias hs="haskell"

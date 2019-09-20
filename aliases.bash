c6a7510ea389bb581d0f66f77f58d38967669367
John Grey <johngrey4296@gmail.com>
2019-09-20 20:04:04 +0100

    removed obsolete aliases

--- aliases.bash
+++ aliases.bash
@@ -1,7 +1,5 @@
 #PERSONAL ALIASES:
 #------------------------------------------------------------
-#switch whereis with which
-alias whereis="which"
 
 #ssh
 alias mini="ssh jg@192.168.1.5"
@@ -9,43 +7,24 @@ alias ubun="ssh john@192.168.1.11"
 alias wind="ssh john@192.168.1.2"
 alias wakemini="wakeonlan A8:8E:24:A3:1A:43"
 
-
 alias master="emacs ~/github/writing/master.org"
 
 alias pServer="python -m http.server 8888 &"
 alias qpServer="(python -m http.server 8888 > /dev/null 2>&1) & && (echo 'Running Quiet Server')"
 alias qpoServer="((python -m http.server 8888 > /dev/null 2>&1) &) && (echo 'Running Quiet Server') && (open http://localhost:8888/)"
 
-alias jges="emacs ~/github/jg_shell_files/"
-alias jgee="cd ~/github/jg_emacs/"
+alias jges="emacs ~/.shell_files/"
 
 alias em="emacs"
 alias lsa="ls -lha"
-alias lsc="lscode"
-alias ls="ls"
 alias lsd="gls -la --group-directories-first"
 alias lsda="ls -la | grep '^d'"
 
-alias lg="logger"
-alias log="logger"
-alias lgf="logger food"
-alias lgw="logger work"
-alias lgp="logger play"
-alias lgh="logger health"
 alias ql="qlmanage -p 2>/dev/null"
 
 alias cd="mycd $1"
 alias cdc="mycdc $1"
 alias cda="mycda $1"
-
-alias jgf="find . -maxdepth 1 -iname $1"
-
-alias macports="port"
-
-alias defaultNode="node"
-#removed --harmony_destructuring
-alias node="node --harmony"
-alias bnode="babel-node"
 
 function mycd(){
     builtin cd "$@" && ls
@@ -59,18 +38,10 @@ function mycda(){
     builtin cd "$@" && lsa
 }
 
+alias jgf="find . -maxdepth 1 -iname $1"
+
 #disk usage:
 alias jgdu="du -hd 1 | sort"
-
-
-#-F --group-directories-first --sort=extension"
-
-#Intern:
-#alias internSetup="mkdir tests; cp ~/programming/js/node_modules/intern/tests/example.intern.js tests/intern.js"
-#alias intern="~/programming/js/node_modules/.bin/intern-client config=tests/intern"
-
-#Markdown alias:
-#alias markdown="markdown_py-2.7"
 
 #sclang for emacs:
 alias sclangel="sclang -d ~/github/.super_collider_classes/ -r -s -i emacs"

7498a3b913c3375f0f9deaa08dee12bea978e3f4
John Grey <johngrey4296@gmail.com>
2019-09-20 20:04:16 +0100

    tidied up main exports

--- exports.bash
+++ exports.bash
@@ -1,66 +1,44 @@
-function setupPath_minus_anaconda(){
-    #PATHS:
-    export PATH=/sbin:/usr/sbin:/bin:/usr/bin
-    export PATH=/usr/local/bin:$PATH
-    export PATH=/Applications/SuperCollider:$PATH
-    export PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
-    export PATH=/Volumes/Documents/Dropbox/Programming/Perl:$PATH
-    export PATH=/Volumes/Documents/github/jg_shell_files:$PATH
-    export PATH=/Applications/Racket\ v6.1.1/bin:$PATH
-    export PATH=/usr/local/go/bin:$PATH
+#PATHS:
+export PATH=/sbin:/usr/sbin:/bin:/usr/bin
+export PATH=/usr/local/bin:$PATH
+export PATH=/Applications/SuperCollider:$PATH
+export PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
+export PATH=/Volumes/Documents/Dropbox/Programming/Perl:$PATH
+export PATH=/Volumes/Documents/github/jg_shell_files:$PATH
+export PATH=/Applications/Racket\ v6.1.1/bin:$PATH
+export PATH=/usr/local/go/bin:$PATH
 
-    #For supercollider:
-    export PATH=/Applications/SuperCollider/SuperCollider.app/Contents/Resources:$PATH
-    export PATH=/Applications/SuperCollider/SuperCollider.app/Contents/MacOS:$PATH
+#Node:
+export PATH=~/.npm-global/bin:$PATH
 
-    #racket:
-    export PATH=/Applications/Racket/bin:$PATH
+#For supercollider:
+export PATH=/Applications/SuperCollider/SuperCollider.app/Contents/Resources:$PATH
+export PATH=/Applications/SuperCollider/SuperCollider.app/Contents/MacOS:$PATH
 
-    #for latex (mactex, when installed):
-    export PATH=/usr/local/texlive/2017/bin/x86_64-darwin:$PATH
+#racket:
+export PATH=/Applications/Racket/bin:$PATH
 
-    #for blender:
-    export PATH=/Applications/blender/blender.app/Contents/MacOS:$PATH
+#for latex (mactex, when installed):
+export PATH=/usr/local/texlive/2017/bin/x86_64-darwin:$PATH
 
-    #For clingo:
-    export PATH=~/clingo:$PATH
+#for blender:
+export PATH=/Applications/blender/blender.app/Contents/MacOS:$PATH
 
-    #for haskell / cabal
-    export PATH=$HOME/Library/Haskell/bin:$PATH
+#For clingo:
+export PATH=~/clingo:$PATH
 
-    #for jacamo (see lang specific)
-    export PATH=$JACAMO_HOME/scripts:$PATH
+#for haskell / cabal
+export PATH=$HOME/Library/Haskell/bin:$PATH
 
-    #for ruby / gems:
-    export PATH=/usr/local/lib/ruby/gems/2.6.0/bin:$PATH
-    export PATH=/usr/local/opt/ruby/bin:$PATH
-}
+#for jacamo (see lang specific)
+export PATH=$JACAMO_HOME/scripts:$PATH
 
-function setupPath_plus_anaconda(){
-    setupPath_minus_anaconda
-    #for anaconda:
-    export PATH=~/anaconda3/bin:$PATH
-}
-#initial setup:
-setupPath_plus_anaconda
-#--
+#for ruby / gems:
+export PATH=/usr/local/lib/ruby/gems/2.6.0/bin:$PATH
+export PATH=/usr/local/opt/ruby/bin:$PATH
 
-function remove_anaconda_for_brew(){
-    setupPath_minus_anaconda
-    export PYTHONPATH=/usr/local/lib/python2.7/site/packages
-    export PYTHONPATH=~/github/otherLibs:$PYTHONPATH #personally installed libs
-    export PYTHONPATH=~/github/:$PYTHONPATH #personally written libs
-    export PYTHONPATH=$JG_PYLIBS:$PYTHONPATH
-    export PYTHONPATH=./:$PYTHONPATH
-}
-
-function reinsert_anaconda_post_brew(){
-    setupPath_plus_anaconda
-    setPy3
-}
-
-#For Tidal:
-export TIDAL_TEMPO_PORT=57120
+#for anaconda:
+export PATH=~/anaconda3/bin:$PATH
 
 #setting up the prompt:
 export PS1='(\j): \D{%D} \A \u:  '
